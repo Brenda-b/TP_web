59400d4af010f1b0f1ba3f689e8d01e797373205
patricio97m <patricio.97m@gmail.com>
2022-11-18 18:27:24 -0300

    carrito

--- js/index.js
+++ js/index.js
@@ -34,7 +34,12 @@ const contenedorProductos = document.getElementById('contenedor-productos');
 
 let carrito= [];
 
-
+window.addEventListener("load", () =>{
+    let cantidad = JSON.parse(sessionStorage.getItem("carrito"))
+    const circulo = document.getElementById("cantidad");
+    circulo.classList.add('cantidad');
+    circulo.innerHTML = cantidad.length;
+})
 
 productos.forEach((articulo) =>{
     const section= document.createElement('section');
@@ -61,11 +66,23 @@ productos.forEach((articulo) =>{
 
     const boton=document.getElementById(`agregar${articulo.id}`);
     
-    boton.addEventListener('click', () => {
+    boton.addEventListener('click', (e) => {
+        e.preventDefault;
         agregarAlCarrito(articulo.id);
-        console.log(carrito)
+        sessionStorage.setItem("carrito", JSON.stringify(carrito));
+        const circulo = document.getElementById("cantidad")
+        circulo.classList.add('cantidad');
+        circulo.innerHTML = carrito.length;
     });
 } );
+
+//Para agregar un cambio
+const agregarAlCarrito= (prodId) => {
+    //traigo el producto en el que el id coincida con el prodId que recibo por parametro
+    const item= productos.find((prod) => prod.id === prodId)
+    carrito.push(item);
+  };
+  
 
 
 
